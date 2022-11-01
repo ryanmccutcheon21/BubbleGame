aee207e1ba16f820585e48b01cabcfed10a156b8
Ryan McCutcheon <ryanmccutcheon5@yahoo.com>
2022-11-01 12:55:44 -0500

    Levels, and random position rounds 2-10

--- src/Circle.java
+++ src/Circle.java
@@ -1,11 +1,13 @@
 import java.awt.*;
 
 class Circle {
-    private int x, y;
+    private int x, y, a, b;
 
-    public Circle(int x, int y) {
+    public Circle(int x, int y, int a, int b) {
         this.x = x;
         this.y = y;
+        this.a = a;
+        this.b = b;
     }
     public void drawCircle(Graphics g){
         g.setColor(Color.red);
@@ -16,5 +18,17 @@ class Circle {
     }
     public int getY() {
         return y;
+    }
+    public int getA() {
+        return a;
+    }
+    public int getB() {
+        return b;
+    }
+    public void setX(int x){
+        this.x = x;
+    }
+    public void setY(int y) {
+        this.y = y;
     }
 }
--- src/PlayingField.java
+++ src/PlayingField.java
@@ -4,15 +4,28 @@ import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.List;
 
-class PlayingField extends JPanel {
+class PlayingField extends JPanel implements ActionListener{
     private double distance;
     private int difficultyNumCircles = 0;
     private boolean playing = false;
     private List<Circle> circles;
 
+    // track # of circles popped
+    private int circlesPopped;
+    // track # of rounds
+    private int rounds = 1;
+
+    // timer delay and set timer variable
+    private int delay = 1000;
+    protected Timer timer;
+    private int roundTime = 15;
+
     public PlayingField(int numCircles) {
+        // create Timer
+        timer = new Timer(delay, this);
         // create circles array
         circles = new ArrayList<Circle> ();
+
         // mouse listener
         addMouseListener(new MouseAdapter(){
             public void mouseClicked(MouseEvent e){
@@ -21,19 +34,41 @@ class PlayingField extends JPanel {
                     for(Circle circle: circles){
                         if(findDistance(circle.getX(), circle.getY(), e.getX()-50, e.getY()-50) < 50){
                             circles.remove(circle);
+                            circlesPopped++;
                             PlayingField.this.repaint();
+                            // end round if all circles are popped
+                            if(circlesPopped % difficultyNumCircles == 0){
+                                rounds++;
+                                timer.stop();
+                                // playing = false;
+                                // check if rounds = 11 to end game
+                                if(rounds == 11){
+                                    JOptionPane.showMessageDialog(null, "You Win!!!", "Congrats!!", JOptionPane.INFORMATION_MESSAGE);
+                                } else {
+                                    JOptionPane.showMessageDialog(null, "Round " + (rounds - 1) + " Over! Ready for Round " + rounds + "?", "Round " + rounds, JOptionPane.INFORMATION_MESSAGE);
+                                    roundTime = 16 - rounds;
+                                    for(int i = 0; i < difficultyNumCircles; i++){
+                                        // random positions for circle starting position rounds 2-10
+                                        circles.add(new Circle((int)Math.floor(Math.random()*(350)+1),(int)Math.floor(Math.random()*(350)+1),(int)Math.floor(Math.random()*(350)+1),(int)Math.floor(Math.random()*(350)+1)));
+                                        PlayingField.this.repaint();
+                                    }
+                                    playing = true;
+                                    timer.start();
+                                }
+                            }
                         }
                     }
                     // check if inside JPanel
                 } else if(e.getX() < 350 && e.getX() > 50 && e.getY() > 50 && e.getY() < 350){
                     // # circles < difficulty, add and draw
-                    circles.add(new Circle(e.getX()-50, e.getY()-50));
+                    circles.add(new Circle(e.getX()-50, e.getY()-50, e.getX()-50, e.getY()-50));
                     PlayingField.this.repaint();
                     difficultyNumCircles++;
-                    // start game if num of circles = difficulty
-                    if(difficultyNumCircles == numCircles){
-                        JOptionPane.showMessageDialog(null, "Are you ready to start?", "Begin", JOptionPane.INFORMATION_MESSAGE);
+                    // start round 1 if num of circles = difficulty
+                    if(difficultyNumCircles % numCircles == 0){
+                        JOptionPane.showMessageDialog(null, "Are you ready to start round " + rounds + "?", "Round " + rounds, JOptionPane.INFORMATION_MESSAGE);
                         playing = true;
+                        timer.start();
                     }
                     // throw error if circle outside of Panel
                 } else {
@@ -57,10 +92,35 @@ class PlayingField extends JPanel {
         return distance;
     }
 
+    // set Panel size
     public Dimension getPreferredSize() {
         return new Dimension(400, 400);
     }
 
+    // Timer function; repeats every 1000 milliseconds
+    public void actionPerformed(ActionEvent e){
+        if(playing){
+            for(Circle circle: circles){
+                int randomX = (int)Math.floor(Math.random()*((circle.getA()+50)-(circle.getA()-50)+(circle.getA()-50))+1);
+                int randomY = (int)Math.floor(Math.random()*((circle.getB()+50)-(circle.getB()-50)+(circle.getB()-50))+1);
+                circle.setX(randomX);
+                circle.setY(randomY);
+                System.out.println(randomX + " : " + randomY);
+                PlayingField.this.repaint();
+            }
+            roundTime--;
+            if(roundTime == 0){
+                JOptionPane.showMessageDialog(null, "Uh Oh! You ran out of time! Go back to the menu and click restart to begin a new game!", "Game Over!!", JOptionPane.INFORMATION_MESSAGE);
+                rounds = 1;
+                roundTime = 15;
+                playing = false;
+                circles.clear();
+                PlayingField.this.repaint();
+            }
+        }
+    }
+
+    // paint circles on Panel
     protected void paintComponent(Graphics g){
         super.paintComponent(g);
         for(Circle circle: circles){
